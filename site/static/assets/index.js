1c9174a93e376b912c02b33aecf8360cf634bebe
kyasu1 <kyasuyako@gmail.com>
2019-12-27 23:11:06 +0900

    fix layout

--- site/static/assets/index.js
+++ site/static/assets/index.js
@@ -45301,7 +45301,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51269" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50815" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
