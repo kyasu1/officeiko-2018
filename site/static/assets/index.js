a16c4444e14c828c14c2fa712691b78bd8f73074
Yasuyuki Komatsubara <kyasuyako@gmail.com>
2020-08-08 10:24:01 +0900

    update market for 2020/08/08

--- site/static/assets/index.js
+++ site/static/assets/index.js
@@ -21934,7 +21934,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54534" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50581" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
